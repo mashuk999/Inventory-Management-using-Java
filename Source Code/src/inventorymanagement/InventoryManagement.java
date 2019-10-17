3ddd8e1132acc0626156671b68b78d606dfb6498
Abhay Nigam <mashuk1998@gmail.com>
2019-10-17 16:17:43 +0530

    initial

---
+++ Source Code/src/inventorymanagement/InventoryManagement.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventorymanagement;
+
+/**
+ *
+ * @author deepak
+ */
+public class InventoryManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+  
+    
+}
